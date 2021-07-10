310b876b1f16fe166c43384cfc9a8a9648527c99
Rohit Patro <rohitpatro6@gmail.com>
2021-07-10 09:39:32 +0530

    Added:UC2 Added

--- hello.java
+++ hello.java
@@ -2,7 +2,10 @@ class hello{
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-int IS_FULL_TIME = 1;
+		int wagePerHour = 20;
+                int fullDayHour = 8;
+                int dailyWage = 0;
+		int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is Present");
